9ada83ec6376d7b06925a463a863323410f5316d
Dylan Sng <dsja612@gmail.com>
2024-10-02 14:49:22 +0800

    add tests for SearchInput component

--- client/src/pages/Contact.test.js
+++ client/src/pages/Contact.test.js
@@ -21,37 +21,37 @@ describe("Contact Component", () => {
   describe("Given the Contact component is rendered", () => {
     
     describe("When the layout is rendered", () => {
-      it("Then it should display the correct title", () => {
+      it("should display the correct title", () => {
         expect(screen.getByText("Contact us")).toBeInTheDocument();
       });
     });
 
     describe("When the contact information is displayed", () => {
-      it("Then it should display the contact heading", () => {
+      it("should display the contact heading", () => {
         expect(screen.getByRole("heading", { name: "CONTACT US" })).toBeInTheDocument();
       });
 
-      it("Then it should display the contact description", () => {
+      it("should display the contact description", () => {
         expect(
           screen.getByText(/For any query or info about product, feel free to call anytime/i)
         ).toBeInTheDocument();
       });
 
-      it("Then it should display the email address", () => {
+      it("should display the email address", () => {
         expect(screen.getByText(/www.help@ecommerceapp.com/i)).toBeInTheDocument();
       });
 
-      it("Then it should display the phone number", () => {
+      it("should display the phone number", () => {
         expect(screen.getByText(/012-3456789/i)).toBeInTheDocument();
       });
 
-      it("Then it should display the toll-free number", () => {
+      it("should display the toll-free number", () => {
         expect(screen.getByText(/1800-0000-0000/i)).toBeInTheDocument();
       });
     });
 
     describe("When the contact image is rendered", () => {
-      it("Then it should display the image with the correct alt text and source", () => {
+      it("should display the image with the correct alt text and source", () => {
         const imgElement = screen.getByAltText("contactus");
         expect(imgElement).toBeInTheDocument();
         expect(imgElement).toHaveAttribute("src", "/images/contactus.jpeg");
