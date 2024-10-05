ed7a9f50c5890f9afd939c173b3f0374dfaacc40
MrTwit99 <wenjueyeo@live.com>
2024-10-05 21:36:33 +0800

    Remove tests for Hook, Context, Form and Route due to 25 tests restraints

--- client/src/context/cart.test.js
+++ client/src/context/cart.test.js
@@ -1,79 +1,79 @@
-import React from "react";
-import { renderHook, act } from "@testing-library/react";
-import { CartProvider, useCart } from "./cart.js";
+// import React from "react";
+// import { renderHook, act } from "@testing-library/react";
+// import { CartProvider, useCart } from "./cart.js";
 
-beforeEach(() => {
-    Storage.prototype.getItem = jest.fn();
-    Storage.prototype.setItem = jest.fn();
-    Storage.prototype.removeItem = jest.fn();
-});
+// beforeEach(() => {
+//     Storage.prototype.getItem = jest.fn();
+//     Storage.prototype.setItem = jest.fn();
+//     Storage.prototype.removeItem = jest.fn();
+// });
 
-describe("Cart Context", () => {
-    // it("should provide the initial cart value from localStorage", () => {
-    //     const mockedCart = [{ id: 1, name: "Product 1", quantity: 1 }];
-    //     localStorage.getItem.mockReturnValueOnce(JSON.stringify(mockedCart));
+// describe("Cart Context", () => {
+//     // it("should provide the initial cart value from localStorage", () => {
+//     //     const mockedCart = [{ id: 1, name: "Product 1", quantity: 1 }];
+//     //     localStorage.getItem.mockReturnValueOnce(JSON.stringify(mockedCart));
 
-    //     const wrapper = ({ children }) => <CartProvider>{children}</CartProvider>;
+//     //     const wrapper = ({ children }) => <CartProvider>{children}</CartProvider>;
 
-    //     const { result } = renderHook(() => useCart(), { wrapper });
-    //     const [cart] = result.current;
+//     //     const { result } = renderHook(() => useCart(), { wrapper });
+//     //     const [cart] = result.current;
 
-    //     expect(cart).toEqual(mockedCart);
-    // });
+//     //     expect(cart).toEqual(mockedCart);
+//     // });
 
-    it("should set a new cart item and update localStorage", () => {
-        const wrapper = ({ children }) => <CartProvider>{children}</CartProvider>;
-        const { result } = renderHook(() => useCart(), { wrapper });
-        const [cart, setCart] = result.current;
-        const newItem = { id: 2, name: "Product 2", quantity: 2 };
+//     it("should set a new cart item and update localStorage", () => {
+//         const wrapper = ({ children }) => <CartProvider>{children}</CartProvider>;
+//         const { result } = renderHook(() => useCart(), { wrapper });
+//         const [cart, setCart] = result.current;
+//         const newItem = { id: 2, name: "Product 2", quantity: 2 };
 
-        expect(cart).toEqual([]);
+//         expect(cart).toEqual([]);
 
-        act(() => {
-            setCart([newItem]);
-        });
+//         act(() => {
+//             setCart([newItem]);
+//         });
 
-        expect(result.current[0]).toEqual([newItem]);
-        expect(localStorage.setItem).toHaveBeenCalledWith("cart", JSON.stringify([newItem]));
-    });
+//         expect(result.current[0]).toEqual([newItem]);
+//         expect(localStorage.setItem).toHaveBeenCalledWith("cart", JSON.stringify([newItem]));
+//     });
 
-    // it("should handle an empty cart in localStorage", () => {
-    //     localStorage.getItem.mockReturnValueOnce(null);
+//     // it("should handle an empty cart in localStorage", () => {
+//     //     localStorage.getItem.mockReturnValueOnce(null);
 
-    //     const wrapper = ({ children }) => <CartProvider>{children}</CartProvider>;
-    //     const { result } = renderHook(() => useCart(), { wrapper });
-    //     const [cart] = result.current;
+//     //     const wrapper = ({ children }) => <CartProvider>{children}</CartProvider>;
+//     //     const { result } = renderHook(() => useCart(), { wrapper });
+//     //     const [cart] = result.current;
 
-    //     expect(cart).toEqual([]);
-    // });
+//     //     expect(cart).toEqual([]);
+//     // });
 
-    it("should log errors occurred with localStorage", () => {
-        localStorage.getItem.mockImplementation(() => {
-            throw new Error("localStorage error");
-        });
+//     it("should log errors occurred with localStorage", () => {
+//         localStorage.getItem.mockImplementation(() => {
+//             throw new Error("localStorage error");
+//         });
 
-        const consoleSpy = jest.spyOn(console, "error").mockImplementation(() => { });
-        const wrapper = ({ children }) => <CartProvider>{children}</CartProvider>;
-        const { result } = renderHook(() => useCart(), { wrapper });
-        const [cart] = result.current;
+//         const consoleSpy = jest.spyOn(console, "error").mockImplementation(() => { });
+//         const wrapper = ({ children }) => <CartProvider>{children}</CartProvider>;
+//         const { result } = renderHook(() => useCart(), { wrapper });
+//         const [cart] = result.current;
 
-        expect(cart).toEqual([]);
-        expect(consoleSpy).toHaveBeenCalledWith("Failed to load cart from localStorage:", expect.any(Error));
+//         expect(cart).toEqual([]);
+//         expect(consoleSpy).toHaveBeenCalledWith("Failed to load cart from localStorage:", expect.any(Error));
 
-        consoleSpy.mockRestore();
-    });
+//         consoleSpy.mockRestore();
+//     });
 
-    // it("should handle invalid JSON in localStorage", () => {
-    //     localStorage.getItem.mockReturnValueOnce("invalid JSON");
+//     // it("should handle invalid JSON in localStorage", () => {
+//     //     localStorage.getItem.mockReturnValueOnce("invalid JSON");
 
-    //     const consoleSpy = jest.spyOn(console, "error").mockImplementation(() => { });
-    //     const wrapper = ({ children }) => <CartProvider>{children}</CartProvider>;
-    //     const { result } = renderHook(() => useCart(), { wrapper });
-    //     const [cart] = result.current;
+//     //     const consoleSpy = jest.spyOn(console, "error").mockImplementation(() => { });
+//     //     const wrapper = ({ children }) => <CartProvider>{children}</CartProvider>;
+//     //     const { result } = renderHook(() => useCart(), { wrapper });
+//     //     const [cart] = result.current;
 
-    //     expect(cart).toEqual([]);
-    //     expect(consoleSpy).toHaveBeenCalled();
+//     //     expect(cart).toEqual([]);
+//     //     expect(consoleSpy).toHaveBeenCalled();
 
-    //     consoleSpy.mockRestore();
-    // });
-});
+//     //     consoleSpy.mockRestore();
+//     // });
+// });
--- client/src/hooks/useCategory.test.js
+++ client/src/hooks/useCategory.test.js
@@ -1,53 +1,53 @@
-import { renderHook, waitFor } from "@testing-library/react";
-import axios from "axios";
-import useCategory from "./useCategory.js";
+// import { renderHook, waitFor } from "@testing-library/react";
+// import axios from "axios";
+// import useCategory from "./useCategory.js";
 
-// Mocking axios.post
-jest.mock("axios");
+// // Mocking axios.post
+// jest.mock("axios");
 
-describe("useCategory", () => {
-    describe("when categories are fetched successfully", () => {
-        let mockedCategories;
+// describe("useCategory", () => {
+//     describe("when categories are fetched successfully", () => {
+//         let mockedCategories;
 
-        const { result } = renderHook(() => useCategory());
-        expect(result.current).toEqual([]);
+//         const { result } = renderHook(() => useCategory());
+//         expect(result.current).toEqual([]);
 
-        beforeEach(() => {
-            mockedCategories = [
-                { id: 1, name: "Electronics", slug: "electronics" },
-                { id: 2, name: "Clothing", slug: "clothing" },
-            ];
+//         beforeEach(() => {
+//             mockedCategories = [
+//                 { id: 1, name: "Electronics", slug: "electronics" },
+//                 { id: 2, name: "Clothing", slug: "clothing" },
+//             ];
 
-            // Mock the axios.get call to return the mocked categories
-            axios.get.mockResolvedValue({
-                data: { category: mockedCategories },
-            });
-        });
+//             // Mock the axios.get call to return the mocked categories
+//             axios.get.mockResolvedValue({
+//                 data: { category: mockedCategories },
+//             });
+//         });
 
-        it('should return categories', async () => {
-            const { result } = renderHook(() => useCategory());
+//         it('should return categories', async () => {
+//             const { result } = renderHook(() => useCategory());
 
-            await waitFor(() => {
-                expect(result.current).toEqual(mockedCategories);
-            });
-        });
-    });
+//             await waitFor(() => {
+//                 expect(result.current).toEqual(mockedCategories);
+//             });
+//         });
+//     });
 
-    describe("when the fetch request fails due to errors", () => {
-        beforeEach(() => {
-            axios.get.mockRejectedValue(new Error("Failed to fetch categories"));
-        });
+//     describe("when the fetch request fails due to errors", () => {
+//         beforeEach(() => {
+//             axios.get.mockRejectedValue(new Error("Failed to fetch categories"));
+//         });
 
-        it("should handle error", async () => {
-            const consoleSpy = jest.spyOn(console, "log").mockImplementation(() => { });
+//         it("should handle error", async () => {
+//             const consoleSpy = jest.spyOn(console, "log").mockImplementation(() => { });
 
-            const { result } = renderHook(() => useCategory());
+//             const { result } = renderHook(() => useCategory());
 
-            await waitFor(() => {
-                expect(consoleSpy).toHaveBeenCalledWith(new Error("Failed to fetch categories"));
-            })
+//             await waitFor(() => {
+//                 expect(consoleSpy).toHaveBeenCalledWith(new Error("Failed to fetch categories"));
+//             })
 
-            consoleSpy.mockRestore();
-        });
-    });
-});
+//             consoleSpy.mockRestore();
+//         });
+//     });
+// });
