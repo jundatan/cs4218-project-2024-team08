c529318d71e686d73fe6d3d93dc377680e531be2
Tan Jun Da <e0726893@u.nus.edu>
2024-10-26 23:09:54 +0800

    Fix category randomizer bug

--- tests/admin.spec.cjs
+++ tests/admin.spec.cjs
@@ -6,57 +6,49 @@ const adminEmail = "Test123@Email.com";
 const adminPassword = "TestTest123123";
 const adminUsername = "TestTest123123";
 
-describe("Admin Create Category", () => {
+const generateCategory = () => {
+  let category = '';
+
+  // Continue generating random strings until we get one long enough
+  while (category.length < 25) {
+      // Generate a base-36 string and append it to category
+      category += Math.random().toString(36).substring(2); // Start from index 2 to skip "0."
+  }
+
+  // Slice to ensure it is exactly 25 characters long
+  return category.substring(0, 25);
+}
+
+describe.serial("Admin Create Category", () => {
   beforeEach(async ({ page }) => {
     await page.goto("http://localhost:3000/");
+    await page.waitForURL("http://localhost:3000/");
     await page.click("text=Login");
-    expect(page.url()).toBe("http://localhost:3000/login");
+    await page.waitForURL("http://localhost:3000/login");
     await page.fill("input[id=exampleInputEmail1]", adminEmail);
     await page.fill("input[id=exampleInputPassword1]", adminPassword);
     await page.click('button.btn-primary:has-text("LOGIN")');
-    await page.waitForNavigation();
-    expect(page.url()).toBe("http://localhost:3000/");
+    await page.waitForURL("http://localhost:3000/");
     await page.click("text=" + adminUsername);
     await page.click("text=Dashboard");
-    expect(page.url()).toBe("http://localhost:3000/dashboard/admin");
+    await page.waitForURL("http://localhost:3000/dashboard/admin");
     await page.click("text=Create Category");
-    expect(page.url()).toBe(
-      "http://localhost:3000/dashboard/admin/create-category"
-    );
-  });
-
-  afterEach(async ({ page }) => {
-    await page.click("text=" + adminUsername);
-    await page.click("text=Logout");
-    expect(page.url()).toBe("http://localhost:3000/login");
+    await page.waitForURL("http://localhost:3000/dashboard/admin/create-category");
   });
 
   test("should create a new category and display it on admin create category page", async ({ page }) => {
-    const category = Math.random().toString(36).substring(10);
-    await page.fill(
-      'input[placeholder="Enter new category"]',
-      category
-    );
+    const category = generateCategory();
+    await page.fill("input[placeholder='Enter new category']", category);
     await page.click('button.btn-primary:has-text("Submit")');
     await expect(
       page.getByText(`${category} is created`)
     ).toBeVisible();
-    const deleterow = await page.locator("tr", {
-      hasText: category,
-    });
-    await expect(deleterow).toBeVisible();
-    await deleterow.locator('button:has-text("Delete")').click();
-    await expect(page.getByText("category is deleted")).toBeVisible();
-    await expect(page.locator("tr", { hasText: category })).toHaveCount(0);
   });
 
   test("should edit a category and display the update on admin create category page", async ({ page }) => {
-    const category = Math.random().toString(36).substring(10);
-    const editCategory = Math.random().toString(36).substring(10);
-    await page.fill(
-      'input[placeholder="Enter new category"]',
-      category
-    );
+    const category = generateCategory();
+    const editCategory = generateCategory();
+    await page.fill("input[placeholder='Enter new category']", category);
     await page.click('button.btn-primary:has-text("Submit")');
     await expect(
       page.getByText(`${category} is created`)
@@ -71,21 +63,11 @@ describe("Admin Create Category", () => {
     await expect(
       page.getByText(`${editCategory} is updated`)
     ).toBeVisible();
-    const deleterow = await page.locator("tr", {
-      hasText: editCategory,
-    });
-    await expect(deleterow).toBeVisible();
-    await deleterow.locator('button:has-text("Delete")').click();
-    await expect(page.getByText("category is deleted")).toBeVisible();
-    await expect(page.locator("tr", { hasText: category })).toHaveCount(0);
   });
 
   test("should delete the category and the category is not displayed on the admin create category page", async ({ page }) => {
-    const category = Math.random().toString(36).substring(10);
-    await page.fill(
-      'input[placeholder="Enter new category"]',
-      category
-    );
+    const category = generateCategory();
+    await page.fill("input[placeholder='Enter new category']", category);
     await page.click('button.btn-primary:has-text("Submit")');
     await expect(
       page.getByText(`${category} is created`)
