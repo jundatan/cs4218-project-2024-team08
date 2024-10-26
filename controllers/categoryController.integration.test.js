e3af727108fcf4de11f0b2b3862d472f631b3352
MrTwit99 <wenjueyeo@live.com>
2024-10-26 16:22:01 +0800

    Add integrated tests for singleCategoryController

--- controllers/categoryController.integration.test.js
+++ controllers/categoryController.integration.test.js
@@ -29,9 +29,9 @@ describe("Integrated tests for Category Controller", () => {
         await mongoServer.stop();
     });
 
-    // beforeEach(async () => {
-    //     await Category.deleteMany();
-    // });
+    beforeEach(async () => {
+        await Category.deleteMany();
+    });
 
     describe("Given the categoryControlller", () => {
         describe("When there is no categories in the database", () => {
@@ -82,6 +82,45 @@ describe("Integrated tests for Category Controller", () => {
                 expect(res.body.success).toBe(false);
                 expect(res.body.message).toBe("Error while getting all categories");
                 expect(res.body.error).toBeDefined();
+            });
+        });
+    });
+
+    describe("Given the singleCategoryController", () => {
+        describe("When no category with the specified slug exists in the database", () => {
+            it("should return 200 and null", async () => {
+                const mockCategories = [
+                    { name: "Electronics", slug: "electronics" },
+                    { name: "Furniture", slug: "furniture" },
+                ];
+
+                await Category.insertMany(mockCategories);
+
+                const res = await request(mockApp).get("/api/v1/category/single-category/door");
+
+                expect(res.statusCode).toBe(200);
+                expect(res.body.success).toBe(true);
+                expect(res.body.message).toBe("Get SIngle Category SUccessfully");
+                expect(res.body.category).toBe(null);
+            })
+        });
+
+        describe("When a category with the specified slug exists in the database", () => {
+            it("should return 200 and the category", async () => {
+                const mockCategories = [
+                    { name: "Electronics", slug: "electronics" },
+                    { name: "Furniture", slug: "furniture" },
+                ];
+
+                await Category.insertMany(mockCategories);
+
+                const res = await request(mockApp).get("/api/v1/category/single-category/electronics");
+
+                expect(res.statusCode).toBe(200);
+                expect(res.body.success).toBe(true);
+                expect(res.body.message).toBe("Get SIngle Category SUccessfully");
+                expect(res.body.category.name).toBe("Electronics");
+                expect(res.body.category.slug).toBe("electronics");
             });
         });
     });
